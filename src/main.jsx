20e2b9e7c2110ee9d1e2658ecca8a75567ac5fba
tortuga <otesh0621@gmail.com>
2025-01-27 18:07:44 +0500

    final

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { FormProvider } from "../src/components/form-components/FormContext";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FormProvider>
+   
       <App />
-    </FormProvider>
+   
   </React.StrictMode>
 );
